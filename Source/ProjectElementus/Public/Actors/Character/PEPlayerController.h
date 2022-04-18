eb342b52a9e99c3709bc1ad3c6fb38469f0d1fea
Lucoiso <lucas.oliveiravb@gmail.com>
2022-04-18 14:27:07 -0300

    Add comments to controller class functions

--- Source/ProjectElementus/Public/Actors/Character/PEPlayerController.h
+++ Source/ProjectElementus/Public/Actors/Character/PEPlayerController.h
@@ -47,14 +47,17 @@ class PROJECTELEMENTUS_API APEPlayerController : public APlayerController, publi
 public:
 	APEPlayerController(const FObjectInitializer& ObjectInitializer);
 
+	/* This function came from IAbilityInputBinding interface, provided by GameFeatures_ExtraActions plugin to manage ability bindings */
 	UFUNCTION(Client, Reliable)
 		virtual void SetupAbilityInputBinding(UInputAction* Action, const int32 InputID) override;
 	virtual void SetupAbilityInputBinding_Implementation(UInputAction* Action, const int32 InputID);
 
+	/* This function came from IAbilityInputBinding interface, provided by GameFeatures_ExtraActions plugin to manage ability bindings */
 	UFUNCTION(Client, Reliable)
 		virtual void RemoveAbilityInputBinding(const UInputAction* Action) const override;
 	virtual void RemoveAbilityInputBinding_Implementation(const UInputAction* Action) const;
 
+	/* Custom function to remove HUD from player screen */
 	UFUNCTION(Client, Reliable)
 		void RemoveHUD();
 	virtual void RemoveHUD_Implementation();
