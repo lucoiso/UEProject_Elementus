3458ff84d693d239e39772b0599d4ff58e438e11
Lucoiso <lucas.oliveiravb@gmail.com>
2022-05-01 23:55:11 -0300

    Add empty inventory plugin

---
+++ Plugins/ElementusInventory/Source/ElementusInventory/Public/ElementusInventory.h
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+class FElementusInventoryModule : public IModuleInterface
+{
+public:
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
