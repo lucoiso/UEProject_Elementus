f2e09d4f289dbb9fd62515598b6d31b4533497f1
Lucas Vilas-Bôas <lucas.oliveiravb@gmail.com>
2022-11-05 11:51:54 -0300

    Fix submodules (lost on LFS reset) (#32)

--- Plugins/UEElementusInventory/Source/ElementusInventoryEditor/Private/ElementusStaticIds.h
+++
@@ -1,9 +0,0 @@
-// Author: Lucas Vilas-Boas
-// Year: 2022
-// Repo: https://github.com/lucoiso/UEElementusInventory
-
-#pragma once
-
-static const FName ElementusEditorTabId("Elementus Inventory");
-static const FName ItemCreatorTabId("Elementus Item Creator");
-static const FName ItemStackPropertyId("PrimaryElementusItemId");
--- Plugins/UEModularFeatures_ExtraActions/Source/Private/AbilityInputBinding.cpp
+++
@@ -1,5 +0,0 @@
-﻿// Author: Lucas Vilas-Boas
-// Year: 2022
-// Repo: https://github.com/lucoiso/UEModularFeatures_ExtraActions
-
-#include "AbilityInputBinding.h"
